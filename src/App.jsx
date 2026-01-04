c24cf54bd9de15e49fd313d81c334ac87e259b7f
Safaa <Safaeettalhi@gmail.com>
2026-01-04 20:31:53 +0100

    Ajout du composant App principal

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Skills from './components/Skills'
+import Education from './components/Education'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="App relative">
+      <div className="particles">
+        {[...Array(20)].map((_, i) => (
+          <div
+            key={i}
+            className="particle"
+            style={{
+              left: `${Math.random() * 100}%`,
+              width: `${Math.random() * 4 + 2}px`,
+              height: `${Math.random() * 4 + 2}px`,
+              animationDelay: `${Math.random() * 20}s`,
+              animationDuration: `${Math.random() * 10 + 15}s`,
+            }}
+          />
+        ))}
+      </div>
+      
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Education />
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
